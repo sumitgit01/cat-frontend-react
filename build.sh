ccf682cceda7fa7557abba72d5c5e8a4f93a23b4
sumitgit01 <sumitjoshi1988@gmail.com>
2026-03-29 19:35:08 +0530

    added images uoload to nexus repo

---
+++ build.sh
@@ -0,0 +1,12 @@
+app_name=$(jq -r '.name' package.json)
+version=$(jq -r '.version' package.json)
+nexusUrl = "http://192.168.68.124:8081/repository/${app_name}"
+docker build -t $app_name:$version .
+
+#docker tag $app_name:$version $account_id.dkr.ecr.ap-southeast-1.amazonaws.com/$cluster_name:$version
+#Finally we push our image to ECR
+#docker push $account_id.dkr.ecr.ap-southeast-1.amazonaws.com/$cluster_name:$version
+docker tag $app_name:$version $nexusUrl:$version
+#Finally we push our image to ECR
+docker push $nexusUrl:$version 
+
