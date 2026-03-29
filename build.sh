8465d1a9309f716469938a022c6fa060240c8b06
sumitgit01 <sumitjoshi1988@gmail.com>
2026-03-29 22:17:03 +0530

    Added stage for nexus credentials

--- build.sh
+++ build.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# Exit on any error
+set -e
+
+# Extract metadata from package.json
 app_name=$(jq -r '.name' package.json)
 version=$(jq -r '.version' package.json)
 
-echo "App: $app_name"
-echo "Version: $version"
+echo "--- Building: $app_name version $version ---"
 
 NEXUS_URL="192.168.68.124:8082"
-REPO_NAME="cat-frontend-react"
 
-docker build -t $app_name:$version .
+# 1. Build the local image
+docker build -t "$app_name:$version" .
 
-docker tag $app_name:$version \
-$NEXUS_URL/$REPO_NAME/$app_name:$version
+# 2. Tag for Nexus
+# Note: Since 8082 is tied to the 'cat-frontend-react' repo, 
+# we don't include the repo name in the path.
+docker tag "$app_name:$version" "$NEXUS_URL/$app_name:$version"
 
-docker push $NEXUS_URL/$REPO_NAME/$app_name:$version
+# 3. Push to Nexus
+echo "--- Pushing to Nexus ---"
+docker push "$NEXUS_URL/$app_name:$version"
+
+echo "--- Build and Push Complete ---"
