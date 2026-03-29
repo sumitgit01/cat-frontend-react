c9a47f377c7093b8c565c672e84d4585a2614305
sumitgit01 <sumitjoshi1988@gmail.com>
2026-03-29 20:56:25 +0530

    Added stage for nexus credentials

--- build.sh
+++ build.sh
@@ -6,16 +6,12 @@ version=$(jq -r '.version' package.json)
 echo "App: $app_name"
 echo "Version: $version"
 
-# ✅ Correct (NO http://)
-NEXUS_URL="192.168.68.124:8070"
+NEXUS_URL="192.168.68.124:8082"
 REPO_NAME="cat-frontend-react"
 
-# Build image
 docker build -t $app_name:$version .
 
-# Tag image
 docker tag $app_name:$version \
 $NEXUS_URL/$REPO_NAME/$app_name:$version
 
-# Push image
 docker push $NEXUS_URL/$REPO_NAME/$app_name:$version
